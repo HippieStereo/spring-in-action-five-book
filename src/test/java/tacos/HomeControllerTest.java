d63c21432080e9859266041a72301e1411bab46d
HippieStereo <stereobroo@yahoo.com>
2021-03-30 17:36:36 +0100

    Update HomeControllerTest.java

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -1,12 +1,12 @@
 package tacos;
 
-import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -15,13 +15,13 @@ import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(HomeController.class)
-class HomeControllerTest {
+public class HomeControllerTest {
 	
     @Autowired
 	private MockMvc mockMvc;
 
 	@Test
-	void testHomePage() throws Exception {
+	public void testHomePage() throws Exception {
 		
 		mockMvc.perform(get("/"))
 	  	.andExpect(status().isOk())
